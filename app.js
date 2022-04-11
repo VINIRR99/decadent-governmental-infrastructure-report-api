1e93fab8850d1918c55df19c9cc078207f551081
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-04-11 09:32:22 -0400

    use cors and app.listen

---
+++ app.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const corsOptions = {
+    credentials: true,
+    origin: [process.env.ACCESS_CONTROL_ALLOW_ORIGIN],
+    methods: ["GET", "POST", "PUT", "DELETE"]
+};
+
+app.use(cors(corsOptions));
+
+app.listen(process.env.PORT, () => console.log(`Server running on port: ${process.env.PORT}
+corsOptions:`, corsOptions));
