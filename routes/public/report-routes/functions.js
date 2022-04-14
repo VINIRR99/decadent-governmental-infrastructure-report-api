46d6b9cd69d58c0fc470a7610629441216964f97
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-04-14 18:05:10 -0400

    order reports and comments by creation

--- routes/public/report-routes/functions.js
+++ routes/public/report-routes/functions.js
@@ -1,12 +1,19 @@
 const Report = require("../../../models/Report.model");
 
 const getAllReports = async () => {
-    const reports = await Report.find({}, { __v: 0 }).populate("user", "username name profileImage").populate({
-        path: "comments",
-        select: "-report -__v",
-        limit: 3,
-        populate: { path: "user", select: "username name profileImage" }
-    });
+    const reportsOrder = [{ boolean: false, order: 1 }, { boolean: true, order: -1 }];
+    const reports = [];
+    for (let i = 0; i < reportsOrder.length; i += 1) {
+        const foundReports = await Report.find({ fixed: reportsOrder[i].boolean }, { __v: 0 }).populate({
+            path: "comments",
+            select: "-report -__v",
+            options: { sort: { createdAt: 1 } },
+            limit: 3,
+            populate: { path: "user", select: "username name profileImage" }
+        }).populate("user", "username name profileImage").sort({ createdAt: reportsOrder[i].order });
+
+        reports.push(...foundReports);
+    };
     return reports;
 };
 
@@ -22,6 +29,7 @@ const findReport = async reportId => {
     const report = await Report.findOne({ _id: reportId }, { __v: 0 }).populate({
         path: "comments",
         select: "-report -__v",
+        options: { sort: { createdAt: 1 } },
         populate: { path: "user", select: "username name profileImage" }
     }).populate("user", "username name profileImage");
     return report;
