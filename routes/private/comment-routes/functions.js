a2d96df73edc98f6910effcc4beb79d8c9aad92a
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-04-23 16:38:56 -0400

    delete a comment

--- routes/private/comment-routes/functions.js
+++ routes/private/comment-routes/functions.js
@@ -4,12 +4,16 @@ const Report = require("../../../models/Report.model");
 
 const commentFunctions = {};
 
-const checkPostNewCommentInput = async (input, reportId, userId) => {
-    if (!(reportId.length === 24) || !/^[a-z0-9]+$/.test(reportId)) {
-        const error = new Error("Provided _id for the report is invalid!");
+const checkId = (idToCheck, idOwner) => {
+    if (!(idToCheck.length === 24) || !/^[a-z0-9]+$/.test(idToCheck)) {
+        const error = new Error(`Provided _id for the ${idOwner} is invalid!`);
         error.status = 400;
         throw error;
     };
+};
+
+const checkCommentInput = async (idToCheck, idOwner, input) => {
+    checkId(idToCheck, idOwner);
 
     const { comment } = await input;
 
@@ -19,34 +23,57 @@ const checkPostNewCommentInput = async (input, reportId, userId) => {
         throw error;
     };
 
-    const checkedInput = { comment, user: userId, report: reportId };
-    return checkedInput;
-};
-
-const checkReportId = async (updatedReport, commentId, userId) => {
-    if (!updatedReport) {
-        await Comment.findOneAndDelete({ _id: commentId, user: userId });
-        const error = new Error("The provided _id for the report does not match any report in our database!");
-        error.status = 404;
-        throw error;
-    };
+    return { comment };
 };
 
 const createComment = async (checkedInput, reportId, userId) => {
     const { _id: commentId } = await Comment.create(checkedInput);
 
-    const updatedReport = await Report.findByIdAndUpdate(reportId, { $push: { comments: commentId } }).select("_id");
-    await checkReportId(updatedReport, commentId, userId);
+    await Report.findByIdAndUpdate(reportId, { $push: { comments: commentId } }).select("_id");
 
     const createdComment = await Comment.findById(commentId, { __v: 0 }).populate("user", "username name profileImage");
     await User.findByIdAndUpdate(userId, { $push: { comments: commentId } });
     return createdComment;
 };
 
-commentFunctions.postNewComment = async (input, reportId, userId) => {
-    const checkedInput = await checkPostNewCommentInput(input, reportId, userId);
+commentFunctions.postNewComment = async (reportId, userId, input) => {
+    const checkedInput = await checkCommentInput(reportId, "report", input);
+    checkedInput.report = reportId;
+    checkedInput.user = userId;
     const createdComment = await createComment(checkedInput, reportId, userId);
     return createdComment;
+};
+
+const updateComment = async (checkedInput, commentId, userId) => {
+    const updatedComment = await Comment.findOneAndUpdate({ _id: commentId, user: userId }, checkedInput, { new: true })
+        .select("-__v").populate("user", "username name profileImage");
+    return updatedComment;
+};
+
+commentFunctions.putComment = async (commentId, userId, input) => {
+    const checkedInput = await checkCommentInput(commentId, "comment", input);
+    const updatedComment = await updateComment(checkedInput, commentId, userId);
+    return updatedComment;
+};
+
+const checkDeleteCommentIdExists = deletedComment => {
+    if (!deletedComment) {
+        const error = new Error("The provided _id for the comment does not match any comment you posted!");
+        error.status = 404;
+        throw error;
+    };
+};
+
+const removeComment = async (commentId, userId) => {
+    const deletedComment = await Comment.findOneAndDelete({ _id: commentId, user: userId }).select("_id");
+    checkDeleteCommentIdExists(deletedComment);
+    await Report.findOneAndUpdate({ comments: { $in: [commentId] } }, { $pull: { comments: commentId } });
+    await User.findOneAndUpdate({ comments: { $in: [commentId] } }, { $pull: { comments: commentId } });
+};
+
+commentFunctions.deleteComment = async (commentId, userId) => {
+    checkId(commentId, "comment");
+    await removeComment(commentId, userId);
 };
 
 module.exports = commentFunctions;
--- routes/private/comment-routes/index.js
+++ routes/private/comment-routes/index.js
@@ -1,5 +1,5 @@
 const { Router } = require("express");
-const { postNewComment } = require("./functions");
+const { postNewComment, putComment, deleteComment } = require("./functions");
 
 const router = Router();
 
@@ -7,11 +7,43 @@ router.post("/:reportId", async (req, res) => {
     try {
         const { reportId } = req.params;
         const { _id: userId } = await req.user;
-        const newComment = await postNewComment(await req.body, reportId, userId);
+        const newComment = await postNewComment(reportId, userId, await req.body);
         res.status(201).json(newComment);
     } catch (error) {
         if (!error.status) error.status = 500;
+        if ((error.status === 500) && (error.name === "ValidationError")) {
+            error.status = 404;
+            error.message = "The provided _id for the report does not match any report in our database!";
+        };
         res.status(error.status).json({ message: "Error while trying to post a new comment!", error: error.message });
+    };
+});
+
+router.put("/:commentId", async (req, res) => {
+    try {
+        const { commentId } = req.params;
+        const { _id: userId } = await req.user;
+        const updatedComment = await putComment(commentId, userId, await req.body);
+        res.status(200).json(updatedComment);
+    } catch (error) {
+        if (!error.status) error.status = 500;
+        if ((error.status === 500) && (error.path === "_id")) {
+            error.status = 404;
+            error.message = "The provided _id for the comment does not match any report you posted!";
+        };
+        res.status(error.status).json({ message: "Error while trying to update a comment!", error: error.message });
+    };
+});
+
+router.delete("/:commentId", async (req, res) => {
+    try {
+        const { commentId } = req.params;
+        const { _id: userId } = await req.user;
+        await deleteComment(commentId, userId);
+        res.status(204).json();
+    } catch (error) {
+        if (!error.status) error.status = 500;
+        res.status(error.status).json({ message: "Error while trying to delete comment!", error: error.message });
     };
 });
 
