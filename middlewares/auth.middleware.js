ff9526a287dc6717f0030fad51381f02ce2d71cb
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-05-04 18:21:23 -0400

    adjust auth.middleware

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -3,9 +3,10 @@ const { verify } = require("jsonwebtoken");
 module.exports = (req, res, next) => {
     try {
         const bearer = req.get("Authorization");
+        if (!bearer) throw new Error("Request without token");
         const token = bearer.split(" ")[1];
         const decodedToken = verify(token, process.env.SECRET_JWT);
         req.user = { ...decodedToken };
         next();
-    } catch (error) {res.status(401).json({ message: "Error in authorization middleware!", error: error.message })};
+    } catch (error) {res.status(401).json({ message: "Unauthorized", error: error.message })};
 };
