a77687e6f2d35c7c2983c55db38721536c3e93cc
Vinicius Leite Batista <viniciusleite384@gmail.com>
2022-05-05 11:13:01 -0400

    upload profileImage

--- routes/private/user-routes/index.js
+++ routes/private/user-routes/index.js
@@ -1,7 +1,20 @@
 const { Router } = require("express");
+const uploadCloud = require("../../../configs/cloudinary.config");
 const { updateUser, deleteUser } = require("./functions");
 
 const router = Router();
+
+router.put("/upload-image", uploadCloud.single("image"), async (req, res) => {
+    try {
+        const { path: profileImage } = req.file;
+        const { _id: userId } = await req.user;
+        const updatedUser = await updateUser({ profileImage }, userId);
+        res.status(200).json(updatedUser);
+    } catch (error) {
+        if (!error.status) error.status = 500;
+        res.status(error.status).json({ message: "Error while uploading user profile image", error: error.message });
+    };
+});
 
 router.put("/", async (req, res) => {
     try {
